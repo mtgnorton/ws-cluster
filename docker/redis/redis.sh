fe1c348123423a234c1931f19532013d5e6c5522
mtgnorton-imac24 <15726204663@163.com>
2024-04-18 14:19:50 +0800

    chore(all): 1. 调整

--- docker/redis/redis.sh
+++ docker/redis/redis.sh
@@ -2,5 +2,5 @@ docker run -d --name redis-container \
   -p 6389:6379 \
   -v ./redis.conf:/usr/local/etc/redis/redis.conf \
   -v ./data:/data \
-  redis:6.0 redis-server /usr/local/etc/redis/redis.conf
+  redis:6.2 redis-server /usr/local/etc/redis/redis.conf
 
