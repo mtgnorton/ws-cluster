8d3d6fa26825b05de662ba7e22a1f64962d356d8
mtgnorton-imac24 <15726204663@163.com>
2024-04-23 15:16:41 +0800

    chore(all): 1. 函数执行超时检测

--- shared/shared.go
+++ shared/shared.go
@@ -1,8 +1,13 @@
 package shared
 
 import (
+	"time"
+
+	"github.com/sasha-s/go-deadlock"
+
 	"github.com/bwmarrin/snowflake"
 	"github.com/go-redis/redis/v8"
+	"github.com/gogf/gf/v2/util/guid"
 	"github.com/mtgnorton/ws-cluster/config"
 	"github.com/mtgnorton/ws-cluster/shared/jwtws"
 )
@@ -22,4 +27,42 @@ func InitSnowflakeRedisJwt(c config.Config) {
 	DefaultRedis = redis.NewClient(&redis.Options{Addr: c.Values().Redis.Addr, Password: c.Values().Redis.Password, Username: c.Values().Redis.User, DB: c.Values().Redis.DB})
 
 	DefaultJwtWs = jwtws.NewJwtWs(c)
+}
+
+type timeoutDetection struct {
+	callIDs map[string]struct{}
+	deadlock.Mutex
+}
+
+var TimeoutDetection = &timeoutDetection{callIDs: make(map[string]struct{})}
+
+// Do 超时检测
+// duration 超时时间
+// timeoutOperate 超时操作
+// begin 操作开始时执行
+// end 操作结束时执行
+func (t *timeoutDetection) Do(duration time.Duration, timeoutOperate func()) (end func()) {
+	// 获取协程id
+	callID := guid.S()
+
+	t.Lock()
+	t.callIDs[callID] = struct{}{}
+	t.Unlock()
+	go func() {
+		time.AfterFunc(duration, func() {
+			t.Lock()
+			defer t.Unlock()
+			if _, ok := t.callIDs[callID]; ok {
+				timeoutOperate()
+				delete(t.callIDs, callID)
+			}
+		})
+	}()
+
+	end = func() {
+		t.Lock()
+		delete(t.callIDs, callID)
+		t.Unlock()
+	}
+	return
 }
