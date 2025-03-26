d449e7ff7dd32dec0044801748d5b6e3713fca23
mtgnorton-imac24 <15726204663@163.com>
2025-03-26 19:51:54 +0800

    fix: 增加 messageChan 缓冲区大小以优化性能

--- core/client/default.go
+++ core/client/default.go
@@ -190,7 +190,7 @@ func NewClient(ctx context.Context, uid string, pid string, cType CType, socket 
 	options = append(options, WithContext(ctx))
 
 	opts := NewOptions(options...)
-	messageChan := make(chan interface{})
+	messageChan := make(chan interface{}, 1000)
 	if cType == CTypeServer {
 		messageChan = make(chan interface{}, 20000)
 	}
